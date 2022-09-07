29cfe0ad050e5446ed08c03324aff177a9430cf3
Jorge Garcia Serna <jorge.garcias@softtek.com>
2022-09-06 22:18:21 -0500

    try2

--- app.py
+++ app.py
@@ -1,36 +1,14 @@
-import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-import av
 import cv2
+import streamlit as st
 
-st.title("My first Streamlit app")
-st.write("Hello, world")
+st.title("Webcam Live Feed")
+run = st.checkbox("Run")
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
 
-
-class VideoProcessor:
-    def __init__(self) -> None:
-        self.threshold1 = 100
-        self.threshold2 = 200
-
-    def recv(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-
-        img = cv2.cvtColor(
-            cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR
-        )
-
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-
-ctx = webrtc_streamer(
-    key="example",
-    video_processor_factory=VideoProcessor,
-    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-)
-if ctx.video_processor:
-    ctx.video_processor.threshold1 = st.slider(
-        "Threshold1", min_value=0, max_value=1000, step=1, value=100
-    )
-    ctx.video_processor.threshold2 = st.slider(
-        "Threshold2", min_value=0, max_value=1000, step=1, value=200
-    )
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write("Stopped")
